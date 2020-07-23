1042cc4641c2aaa3da7d771b1cc8c0df5707eefc
Mahavir Munot <mahavir1408@gmail.com>
2020-07-23 22:34:22 +0530

    Code to practice js methods

---
+++ basics/index.js
@@ -0,0 +1,14 @@
+const strings = ['a', 'b', 'c', 'd'];
+console.log('Initial', strings);
+
+//push: push an element at the end of an array
+strings.push('e');
+console.log('push', strings);
+
+//pop: remove an element fromt the end of an array
+strings.pop();
+console.log('pop', strings);
+
+//unshift: remove an element fromt the top of an array
+strings.unshift();
+console.log('unshift', strings);
